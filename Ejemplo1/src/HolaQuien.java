909d3f4e23cc789bf32edb4f106e99e41bc3952e
wjpe <86629087+wjpe@users.noreply.github.com>
2021-07-01 21:40:55 -0500

    Update HolaQuien.java

--- Ejemplo1/src/HolaQuien.java
+++ Ejemplo1/src/HolaQuien.java
@@ -1,12 +1,11 @@
 import java.io.BufferedReader;
-
-import jdk.internal.org.jline.utils.InputStreamReader;
+import java.io.InputStreamReader;
 
 public class HolaQuien {
-   public static void main(String[] args) {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       System.out.println("Por favor digite su nombre: ");
-       String nombre = br.readLine();
-       System.out.println(nombre);
-   } 
+    public static void main(String[] args) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Por favor digite su nombre: ");
+    String nombre = br.readLine();
+    System.out.println(nombre);
+    } 
 }
