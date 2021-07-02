074e04ea9918d816303d26aebe2c94f3b4656bcb
wjpe <86629087+wjpe@users.noreply.github.com>
2021-07-02 15:57:11 -0500

    Agragado el archivo otroNumero.java

---
+++ Ejemplo1/src/otroNumero.java
@@ -0,0 +1,17 @@
+/**
+ * otroNumero
+ */
+
+import java.util.Scanner;
+
+public class otroNumero {
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese otro numero");
+
+        Scanner sc = new Scanner(System.in);
+        int numero = sc.nextInt();
+
+        System.out.println("el numero es " + numero);
+    }
+}
