89e555e15a577929468c0ebeb4496270849cca9c
wjpe <86629087+wjpe@users.noreply.github.com>
2021-07-01 21:46:02 -0500

    Julio 1 2021

---
+++ Ejemplo1/src/java29b.java
@@ -0,0 +1,11 @@
+public class java29b {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a+5);
+        System.out.println(a-5);
+        System.out.println(a*5);
+        System.out.println(a/5);
+        System.out.println(a%5);
+    }
+    
+}
